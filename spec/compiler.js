64cb0907e763c5037ceb9b85db25216e0272b96a
Gerard Paapu <sharkbrainguy@gmail.com>
2011-11-14 08:49:38 +1300

    more assertions for the compiler specs

--- spec/compiler.js
+++ spec/compiler.js
@@ -15,3 +15,14 @@ var assert = require('assert'),
     console.log(src);
     console.log( JSON.stringify( compile(src) ) );
 });
+
+assert.equal(compile('"foo"'), '"foo"');
+assert.equal(compile('9'), '9');
+
+// Compiling identifiers
+assert.equal(compile('foo'), 'foo');
+assert.equal(compile('foo!'), 'foo_bang_');
+assert.equal(compile('export'), '_$export');
+
+assert.equal(compile('(set! foo bar)'), 'foo = bar');
+assert.equal(compile('(if foo bar baz)'), 'foo?bar:baz');
