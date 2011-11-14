d7695143f7888336226a5f7535d0ecf53c98753c
Gerard Paapu <sharkbrainguy@gmail.com>
2011-11-14 22:32:39 +1300

    added tests for Number#zero? and Object#null?

--- spec/run.js
+++ spec/run.js
@@ -4,3 +4,7 @@ var assert = require('assert'),
 assert.equal(run('(3 + 4)'), 7);
 assert.deepEqual(run('(#{foo 1 bar 2} keys)'), ['foo', 'bar']);
 assert.equal(run('((function (a) (a + a)) call null 4)'), 8);
+assert.equal(run('null.null?'), true); 
+assert.equal(run('#{}.null?'), false); 
+assert.equal(run('3.zero?'), false);
+assert.equal(run('0.zero?'), true);
