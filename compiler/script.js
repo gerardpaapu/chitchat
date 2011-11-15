41f8e5c9fbdc7c0859878d24ef5f606c305b654b
Gerard Paapu <sharkbrainguy@gmail.com>
2011-11-16 10:00:58 +1300

    beautify the javascript output

--- compiler/script.js
+++ compiler/script.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 var compile = require('./compile.js').compile, 
+    jsp = require('uglify-js').parser,
+    pro = require('uglify-js').uglify,
     src = '';
 
 process.stdin.resume();
@@ -9,6 +11,13 @@ process.stdin.on('data', function (data) {
 });
 
 process.stdin.on('end', function (data) {
+    var js, ast, output;
+
+    js = compile(src);
+    ast = jsp.parse(js);
+    ast = pro.ast_squeeze(ast, { make_seqs: false });
+    output = pro.gen_code(ast, { beautify: true });
+
     process.stdout.write('// Chitchat Source\n');
     process.stdout.write('// -----------------\n');
     src.split('\n').forEach(function (line) {
@@ -17,8 +26,7 @@ process.stdin.on('end', function (data) {
     process.stdout.write('// -----------------\n');
     process.stdout.write('// Javascript Output\n');
     process.stdout.write('// -----------------\n');
-
     process.stdout.write('var CHITCHAT = require("./runtime/index.js");\n');
-    process.stdout.write(compile(src));
+    process.stdout.write(output);
     process.stdout.write('\n');
 });
