9eea9770bb6dc5b48b608317363726bafd314445
Gerard <gerard@gerardpaapu.com>
2011-12-22 09:33:04 +1300

    Started add language primitives
    --HG--
    extra : rebase_source : 7084fb719339cd95ec3cec89434c5324ac256a25

---
+++ compiler/primitive.js
@@ -0,0 +1,40 @@
+var assert = require('assert'); 
+
+var Primitive = function (emit) {
+    this.emit = emit;
+};
+
+// emit: stx, env -> javascript 
+Primitive.prototype.emit = function (stx, env, compile) {
+    throw new Error("Not Implemented");
+}; 
+
+primitives = {
+    'with-modules': function (stx, env, compile) {
+        
+    },
+
+    'localize': function (stx, env, compile) {
+
+    },
+
+    'function': function (stx, env, compile) {
+
+    },
+
+    'if': function (stx, env, compile) {
+        assert.ok(stx.length === 3);
+
+        return format('($0?$1:$2)', 
+                      compile(stx[1], env),
+                      compile(stx[2], env));
+    },
+
+    'try': function (stx, env, compile) {
+    },
+
+    'throw': function (stx, env, compile) {
+        
+        return format('throw $0', compile(stx[1]));
+    }
+};
